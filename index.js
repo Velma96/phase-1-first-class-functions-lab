eee8ac567b8e0bae961b82cbe56cf0d960f32618
phoebe velma awuor <awuorphoebi@gmail.com>
2024-11-27 21:32:13 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+  return drivers.slice(0, 2); 
+}
+function returnLastTwoDrivers(array) {
+    return array.slice(-2); 
+}
+function returnFirstTwoDrivers(array) {
+    return array.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(array) {
+    return array.slice(-2);
+  }
+  
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3)
+
+  function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers);
+  }
